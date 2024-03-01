0d8e4b63bf9c5da09a1e14123c72e9ead0f74cbb
Neel <neelmehta041105@gmail.com>
2024-03-01 14:59:50 +0530

    f

---
+++ T_login.php
@@ -0,0 +1,43 @@
+<?php
+
+$db_host = "localhost";
+$db_name = "project";
+$db_user = "neel";
+$db_password = "tech@123";
+//connects database
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+//till here all code goes in all php files
+
+
+//login processing starts here
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_real_escape_string($conn, $_POST['loginUsername']);
+    $password = mysqli_real_escape_string($conn, $_POST['loginPassword']);
+    header('Location: jstt.php?data=' . $username);
+   
+    $sql = "SELECT * FROM student WHERE SAP_ID = '$username' AND Password = '$password'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+
+       
+        // if (password_verify($password, $row['Password'])) {
+            
+            header('Location: Student/Home.html'); 
+        } else {
+            echo '<script>window.prompt("Username or password is incorrect.");</script>';
+        }
+    }
+    else {
+        echo '<script>window.prompt("Username or password is incorrect.");</script>';
+    }
+//line below goes everywhere
+    mysqli_close($conn);
+// }
+?>
---
+++ T_signup.php
@@ -0,0 +1,42 @@
+<?php
+$db_host = "localhost";
+$db_name = "project";
+$db_user = "neel";
+$db_password = "tech@123";
+
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$fname = $_POST["signupFname"];
+$lname = $_POST["signupLname"];
+$Div = $_POST["signupDiv"];
+$batch = $_POST["signupBatch"];
+$course = $_POST["signupCourse"];
+$sapid = $_POST["signupSapID"];
+$year = $_POST["options"];
+$password = $_POST["signupPassword"];
+$cpassword = $_POST["signupPass"];
+
+if ($password != $cpassword) {
+    echo "<script>window.alert(`Password Does Not Match`)</script>";
+} else {
+    // Hash the password
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // Prepare the SQL statement using a prepared statement
+    $sql= "INSERT INTO student(SAP_ID,`First Name`,`Last Name`,Division,Batch,Course,yr,Password)
+    VALUES ('$sapid','$fname','$lname','$Div','$batch','$course','$year','$password')";
+
+    $result = mysqli_query($conn, $sql);
+    header('Location: Student/Home.html'); 
+
+}
+
+
+
+
+mysqli_close($conn);
+?>
