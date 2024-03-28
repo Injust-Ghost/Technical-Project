0d4de9e02ce6873a67001b0787c64eb68cdda0bd
Neel <neelmehta041105@gmail.com>
2024-03-28 21:07:08 +0530

    kk

--- Admin/floor5.php
+++ Admin/floor5.php
@@ -1,147 +1,147 @@
-<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="floor.css">
-</head>
-<body>
-    <script>
-        var value= sessionStorage.getItem('Value');
-        document.getElementById(value).style.backgroundColor='Cyan';
-    </script>
-    <table height="10%" width="100%" border="0" bgcolor="bisque">
-        <col width="570"><col width="400"><col width="300">
-            <thead>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="floor.css">
+    </head>
+    <body>
+        <script>
+            var value= sessionStorage.getItem('Value');
+            document.getElementById(value).style.backgroundColor='Cyan';
+            window.alert(value);
+        </script>
+        <table height="10%" width="100%" border="0" bgcolor="bisque">
+            <col width="570"><col width="400"><col width="300">
+                <thead>
+                    <tr>
+                        <th><a href="../Student/Home.html" target="_parent">ON THE GO</a></th>
+                        <th>DETAILS</th>
+                        <th align="right"><img src="../Student/Images/account.png" height="50" width="50"><img src></th>
+                    </tr>
+                </thead>
+        </table>
+        <table height="100" width="100%" border="1">
+            <tbody>
                 <tr>
-                    <th><a href="../Student/Home.html" target="_parent">ON THE GO</a></th>
-                    <th>DETAILS</th>
-                    <th align="right"><img src="../Student/Images/account.png" height="50" width="50"><img src></th>
+                    <td width="600">Faculty Room 502</td>
+                    <td width="200">Washrooms</td>
+                    <td width="100">Stairs</td>
+                    <td width="600">Faculty Room 501</td>
                 </tr>
-            </thead>
-    </table>
-    <table height="100" width="100%" border="1">
-        <tbody>
-            <tr>
-                <td width="600">Faculty Room 502</td>
-                <td width="200">Washrooms</td>
-                <td width="100">Stairs</td>
-                <td width="600">Faculty Room 501</td>
-            </tr>
-        </tbody>
-    </table>
-    <br><br><br><br>
-    <table border="0" width="100%">
-        <tbody>
-            <tr>
-                <td width="300" rowspan="6">
-                    <table border="1" width="100%">
-                        <tbody>
-                            <tr>
-                                <td height="125" id="CR503">CR 503</td>
-                            </tr>
-                            <tr>
-                                <td height="85" id="CL501">CL 501</td>
-                            </tr>
-                            <tr>
-                                <td height="85" id="TR501">TR 501</td>
-                            </tr>
-                            <tr>
-                                <td height="125" id="BOYCR">Boys Common Room</td>
-                            </tr>
-                            <tr>
-                                <td height="50">Washrooms</td>
-                            </tr>
-                            <tr>
-                                <td height="50">Exit</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
-                <td rowspan="6" width="200"></td>
-                <td rowspan="6">
-                    <table border="1" width="100%" height="100%">
-                        <tbody>
-                            <tr>
-                                <td height="50" width="500" colspan="2">Lifts</td>
-                            </tr>
-                            <tr>
-                                <td width="250" height="120" id="CR508">CR 508</td>
-                                <td width="250" height="120" id="CR504">CR 504</td>
-                            </tr>
-                            <tr>
-                                <td colspan="2" height="182">FLOOR 5</td>
-                            </tr>
-                            <tr>
-                                
-                                    <td height="120" id="CR507">CR 507</td>
-                                    <td height="120" id="CR506">CR 506</td>
-                                
-                            </tr>
-                            <tr>
-                                <td height="50" colspan="2">Lifts</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
-                <td rowspan="6" width="200"></td>
-                <td width="300" rowspan="6">
-                    <table border="1" width="100%">
-                        <tbody>
-                            <tr>
-                                <td height="80" id="CL502">CL 502</td>
-                            </tr>
-                            <tr>
-                                <td height="80" id="CL503">CL 503</td>
-                            </tr>
-                            <tr>
-                                <td height="50">Exit</td>
-                            </tr>
-                            <tr>
-                                <td height="130" id="CR504">CR 504</td>
-                            </tr>
-                            <tr>
-                                <td height="130" id="CR505">CR 505</td>
-                            </tr>
-                            <tr>
-                                <td height="50">Exit</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <br><br><br><br>
-    <table width="100%" border="0">
-        <tbody>
-            <tr>
-                <td colspan="2">
-                    <table height="100" width="100%" border="1">
-                        <tbody>
-                            <tr>
-                                    <td width="300" id="CR502">CR 502</td>
-                                    <td width="300" id="CR501">CR 501</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>
-                <td height="100" width="300">
+            </tbody>
+        </table>
+        <br><br><br><br>
+        <table border="0" width="100%">
+            <tbody>
+                <tr>
+                    <td width="300" rowspan="6">
+                        <table border="1" width="100%">
+                            <tbody>
+                                <tr>
+                                    <td height="125" id="CR503">CR 503</td>
+                                </tr>
+                                <tr>
+                                    <td height="85" id="CL501">CL 501</td>
+                                </tr>
+                                <tr>
+                                    <td height="85" id="TR501">TR 501</td>
+                                </tr>
+                                <tr>
+                                    <td height="125" id="BOYCR">Boys Common Room</td>
+                                </tr>
+                                <tr>
+                                    <td height="50">Washrooms</td>
+                                </tr>
+                                <tr>
+                                    <td height="50">Exit</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                    <td rowspan="6" width="200"></td>
+                    <td rowspan="6">
+                        <table border="1" width="100%" height="100%">
+                            <tbody>
+                                <tr>
+                                    <td height="50" width="500" colspan="2">Lifts</td>
+                                </tr>
+                                <tr>
+                                    <td width="250" height="120" id="CR508">CR 508</td>
+                                    <td width="250" height="120" id="CR504">CR 504</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" height="182">FLOOR 5</td>
+                                </tr>
+                                <tr>
+                                    
+                                        <td height="120" id="CR507">CR 507</td>
+                                        <td height="120" id="CR506">CR 506</td>
+                                    
+                                </tr>
+                                <tr>
+                                    <td height="50" colspan="2">Lifts</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                    <td rowspan="6" width="200"></td>
+                    <td width="300" rowspan="6">
+                        <table border="1" width="100%">
+                            <tbody>
+                                <tr>
+                                    <td height="80" id="CL502">CL 502</td>
+                                </tr>
+                                <tr>
+                                    <td height="80" id="CL503">CL 503</td>
+                                </tr>
+                                <tr>
+                                    <td height="50">Exit</td>
+                                </tr>
+                                <tr>
+                                    <td height="130" id="CR504">CR 504</td>
+                                </tr>
+                                <tr>
+                                    <td height="130" id="CR505">CR 505</td>
+                                </tr>
+                                <tr>
+                                    <td height="50">Exit</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <br><br><br><br>
+        <table width="100%" border="0">
+            <tbody>
+                <tr>
+                    <td colspan="2">
+                        <table height="100" width="100%" border="1">
+                            <tbody>
+                                <tr>
+                                        <td width="300" id="CR502">CR 502</td>
+                                        <td width="300" id="CR501">CR 501</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                    <td height="100" width="300">
 
-                </td>
-                <td colspan="2">
-                    <table height="100" width="100%" border="1">
-                        <tbody>
-                            <tr>
-                                <td width="200" id="CL506">CL 506</td>
-                                <td width="200" id="CL505">CL 505</td>
-                                <td width="200" id="CL504">CL 504</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </td>                               
-            </tr>
-        </tbody>
-    </table>
-</body>
+                    </td>
+                    <td colspan="2">
+                        <table height="100" width="100%" border="1">
+                            <tbody>
+                                <tr>
+                                    <td width="200" id="CL506">CL 506</td>
+                                    <td width="200" id="CL505">CL 505</td>
+                                    <td width="200" id="CL504">CL 504</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>                               
+                </tr>
+            </tbody>
+        </table>
+    </body>
 </html>
